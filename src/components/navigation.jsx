1a4f5273b02234ed3fbb3b6b3b8fa17ab8a80c4d
naisydaze <shanaisyz@gmail.com>
2025-08-17 22:11:45 +0800

    second commit for progress page

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -17,14 +17,25 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
-          <a className="navbar-brand page-scroll" href="#page-top">
+          {/* <a className="navbar-brand page-scroll" href="#page-top">
             <img
               src="./img/logo/Navbar-logo.png"
               alt="SY Ideation Works Logo"
               className="navbar-brand-img"
-              style={{ height: "200px", verticalAlign: "left", marginTop: "-93px" ,  marginInline: "-500px" }} // Adjust height as needed
+              // style={{ height: "200px", verticalAlign: "left", marginTop: "-93px" ,  marginInline: "-500px" }} // Adjust height as needed
+              style={{ height: "20px", width: "20px", marginRight:"auto" }} // Adjust height as needed
             />
-          </a>
+          </a> */}
+          <nav className="navbar">
+            <a className="navbar-brand page-scroll" href="#page-top">
+              <img
+                src="./img/logo/Navbar-logo.png"
+                alt="SY Ideation Works Logo"
+                className="navbar-brand-img"
+                style={{ height: "225px", width: "auto" }} // scales better than px
+              />
+            </a>
+          </nav>
           {/* <a className="navbar-brand page-scroll" href="#page-top">
               <span className="sy-brand">SY</span> 
               <span className="ideation-brand"> Ideation Works</span>
