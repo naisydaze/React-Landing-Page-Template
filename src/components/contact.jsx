04eb6a1d0259645cd9b148d9fee7a58622d2872b
naisydaze <shanaisyz@gmail.com>
2025-08-18 11:25:04 +0800

    editting contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -41,14 +41,14 @@ export const Contact = (props) => {
         <div className="container">
           <div className="col-md-8">
             <div className="row">
-              <div className="section-title">
+              {/* <div className="section-title">
                 <h2>Get In Touch</h2>
                 <p>
                   Please fill out the form below to send us an email and we will
                   get back to you as soon as possible.
                 </p>
-              </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
+              </div> */}
+              {/* <form name="sentMessage" validate onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-md-6">
                     <div className="form-group">
@@ -95,34 +95,36 @@ export const Contact = (props) => {
                 <button type="submit" className="btn btn-custom btn-lg">
                   Send Message
                 </button>
-              </form>
+              </form> */}
             </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
+          <div className="row justify-content-center">
+            <div className="col-md-3 contact-info">
+              <div className="contact-item">
+                <h3>Get in touch to learn more about us.</h3>
+                <p>
+                  <span>
+                    <i className="fa fa-map-marker"></i> Address
+                  </span>
+                  {props.data ? props.data.address : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-phone"></i> Phone
+                  </span>{" "}
+                  {props.data ? props.data.phone : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-envelope-o"></i> Email
+                  </span>{" "}
+                  {props.data ? props.data.email : "loading"}
+                </p>
+              </div>
             </div>
           </div>
           <div className="col-md-12">
